1c9364657377225ae2151efae46f9100afa4fd6d
Neruthes <i@neruthes.xyz>
2023-01-16 00:39:03 +0000

    Regular update '2023-01-16T00:39:03+00:00'

--- cron.sh
+++ cron.sh
@@ -15,26 +15,41 @@ WRITE_OSSLIST=n minoss _dist/tgmsg.txt
 
 
 
-echo -e "*Build job completed at $(date '+%F %T')*\n" > .tmp/notif.txt
+echo -e "<strong>Build job completed at $(date '+%F %T')</strong><br><br>" > .tmp/notif.html
 
 if grep -qsi error .tmp/buildlog.txt; then
-    echo -e "Found some error in the build log." >> .tmp/notif.txt
+    echo -e "Found some error in the build log.<br><br>" >> .tmp/notif.html
 else
-    echo -e "Everything looks good.\n" >> .tmp/notif.txt
+    echo -e "Everything looks good.<br><br>" >> .tmp/notif.html
 fi
-minoss .tmp/buildlog.txt | grep '^FINAL_HTTP_URL=' | cut -d= -f2 >> .tmp/notif.txt 2>/dev/null
+echo -e "<a href='https://minio-zt.neruthes.xyz/oss/keep/webdigest/buildlog.txt--a43ccfb78e5b960ad7a2b6226572bf19.txt'>
+    https://minio-zt.neruthes.xyz/oss/keep/webdigest/buildlog.txt--a43ccfb78e5b960ad7a2b6226572bf19.txt
+</a><br><br>" >> .tmp/notif.html 2>/dev/null
 
-echo -e '\n''TgMsg is ready:
+echo -e "TgMsg is ready:<br>
+<a href='https://minio-zt.neruthes.xyz/oss/keep/webdigest/tgmsg.txt--66cefd096d8849b060a4b71eeba91963.txt'>
+    https://minio-zt.neruthes.xyz/oss/keep/webdigest/tgmsg.txt--66cefd096d8849b060a4b71eeba91963.txt
+</a>
+<br><br>" >> .tmp/notif.html
 
-https://minio-zt.neruthes.xyz/oss/keep/webdigest/tgmsg.txt--66cefd096d8849b060a4b71eeba91963.txt' >> .tmp/notif.txt
+pdfurl="https://nas-public.neruthes.xyz:2096/webdigest-07f285cda0d2dd34bc7a4d07/$(find _dist/issue -name '*.pdf' | sort -r | head -n1)"
+echo -e "Artifact:<br>
+<a href='$pdfurl'>$pdfurl</a>" >> .tmp/notif.html
 
-tgbot-msg /home/neruthes/DEV/clinotifbot-tg $(pasm p tgid) < .tmp/notif.txt
+
+cp .tmp/notif.html _dist/notif.html
+
+shareDirToNasPublic
+
+MARKUP=HTML tgbot-msg /home/neruthes/DEV/clinotifbot-tg $(pasm p tgid) <<< "Web Digest build job is over.
+
+<a herf='https://nas-public.neruthes.xyz:2096/webdigest-07f285cda0d2dd34bc7a4d07/_dist/notif.html'>https://nas-public.neruthes.xyz:2096/webdigest-07f285cda0d2dd34bc7a4d07/_dist/notif.html</a>"
 
 
 
 ### And play with experimental ApubNode instance
-cd /home/neruthes/DEV/mypubnode
-apubnode newnote webdigest '' <<< /home/neruthes/DEV/webdigest/_dist/tgmsg.html
+# cd /home/neruthes/DEV/mypubnode
+# apubnode newnote webdigest '' <<< /home/neruthes/DEV/webdigest/_dist/tgmsg.html
 # bash build.sh cf
 # git add .
 # git commit -m 'Automatic commit '"$(date '+%F %T')"
